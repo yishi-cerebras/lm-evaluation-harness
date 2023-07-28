980f988c25c2205304f465280d80da4a48e2bc59
Naoki Orii <mrorii@gmail.com>
2023-07-28 00:28:33 +0000

    Add 4 new evaluation tasks for 6 JP models

--- models/community/cyberagent-open-calm-instruct-3b_1.3.0/harness.sh
+++ models/community/cyberagent-open-calm-instruct-3b_1.3.0/harness.sh
@@ -2,11 +2,11 @@
 set -eu
 PROJECT_DIR=""
 MODEL_ARGS="pretrained=${PROJECT_DIR}/instruction_tuning/outputs/open-calm-instruct-3b_1.3.0,tokenizer=cyberagent/open-calm-3b"
-TASK="jsquad-1.1-0.3,jcommonsenseqa-1.1-0.3,jnli-1.1-0.3,marc_ja-1.1-0.3"
+TASK="jcommonsenseqa-1.1-0.3,jnli-1.1-0.3,marc_ja-1.1-0.3,jsquad-1.1-0.3,jaqket_v2-0.1-0.3,xlsum_ja-1.0-0.3,xwinograd_ja,mgsm-1.0-0.3"
 python main.py \
     --model hf-causal \
     --model_args $MODEL_ARGS \
     --tasks $TASK \
-    --num_fewshot "2,3,3,3" \
+    --num_fewshot "3,3,3,2,1,1,0,5" \
     --device "cuda" \
-    --output_path "models/open-calm-instruct-3b_1.3.0/result.json"
+    --output_path "models/community/cyberagent-open-calm-instruct-3b_1.3.0/result.json"
